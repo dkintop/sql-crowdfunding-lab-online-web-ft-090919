e017a353ee80e4ce3ac814600cbe7c18ec676497
Doug Kintop <dkintop@hotmail.com>
2019-10-09 13:23:15 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE project (id TEXT PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT)  
+CREATE TABLE project (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT)  
