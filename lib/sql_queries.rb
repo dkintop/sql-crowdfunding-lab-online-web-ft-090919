69ad7f04204262bcff69ec885d58c510c43089af
Doug Kintop <dkintop@hotmail.com>
2019-10-09 14:00:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,8 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount) AS total_pledges FROM users 
   INNER JOIN pledges
-  ON users.id = pledges.user_id"
+  ON users.id = pledges.user_id
+  GROUP BY 
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
